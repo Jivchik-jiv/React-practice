828cdd1ece22a8c78d803da9b21f2b5125197864
Jivchik-jiv <jivchik.mail@gmail.com>
2021-05-07 14:51:01 +0300

    Lesson 3 - Added Couter and ColorPicker

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import ColorPicker from './content/ColorPicker/ColorPicker';
+import Counter from './content/Counter/Counter';
 import Layout from './content/Layout/Layout';
 import Profile from './content/Profile/Profile';
 import user from './content/user.json'
+
+const colorPickerOptions = [
+  {label: "crimson",color: "#DC143C"},
+  {label: "darkorange",color: "#FF8C00"},
+  {label: "khaki",color: "#F0E68C"},
+  {label: "lime",color: "#00FF00"},
+  {label: "teal",color: "#008080"},
+  {label: "navy",color: "#000080"},
+  {label: "indigo",color: "#4B0082"},
+]
 
 
 const App = () => {
@@ -10,6 +22,8 @@ const App = () => {
     <>
     <Layout >
       <Profile {...user}/>
+      <ColorPicker colors = {colorPickerOptions}/>
+      <Counter />
     </Layout>
     </>
   )
---
+++ react-practice/src/content/Counter/Counter.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Counter.module.css'
+
+class Counter extends React.Component {
+
+    state = {
+        counterValue: 0,
+    }
+
+    handleIncrement = () =>{
+        this.setState(prevState=> ({counterValue: prevState.counterValue + 1}))
+    } 
+
+    handleDecrement = () =>{
+        this.setState(prevState=> ({counterValue: prevState.counterValue - 1}))
+    } 
+
+    render(){
+        const counter = this.state.counterValue;
+        return(
+            <div className = {styles.counter}>
+                <h2>Counter</h2>
+                <p>{counter}</p>
+                <button className = {styles.btn}
+                        onClick = {this.handleIncrement}
+                >Add</button>
+                <button className = {styles.btn}
+                        onClick = {this.handleDecrement}
+                >Subtract</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
